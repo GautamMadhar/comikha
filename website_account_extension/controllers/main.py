6b1da1d117dd3e156cf3f203ff8ca4486444571b
ansh <Ansh Shah>
2024-06-30 23:59:39 +0530

    made changes

--- website_account_extension/controllers/main.py
+++ website_account_extension/controllers/main.py
@@ -1,16 +1,25 @@
 from odoo import http
 from odoo.http import request
+import base64
+
+
 
 class WebsiteForm(http.Controller):
     @http.route('/comikha-contact-form', type="http", auth="user", website=True, csrf=False)
     def submit_form(self, **kw):
         if request.httprequest.method == 'POST':
             # Extract form data
-            profile_photo = kw.get('picture')
-            i_card = kw.get('i_card')
-            trader_card = kw.get('trader_card')
-            miner_card = kw.get('miner_card')
+            # profile_photo = kw.get('picture')
+            # i_card = kw.get('i_card')
+            # trader_card = kw.get('trader_card')
+            # miner_card = kw.get('miner_card')
+            # picture_data = self.process_uploaded_file(profile_photo)
+            # id_card_data = self.process_uploaded_file(i_card)
+            # trader_card_data = self.process_uploaded_file(trader_card)
+            # miner_card_data = self.process_uploaded_file(miner_card)
+        
             values = {
+                # 'image_1920': picture_data,
                 'name': kw.get('name'),
                 'birth_date': kw.get('birth_date'),
                 'nationality': kw.get('nationality'),
@@ -21,8 +30,20 @@ class WebsiteForm(http.Controller):
                 'occupation': kw.get('occupation'),
                 'zip': kw.get('zipcode'),
                 'email': kw.get('email'),
+                'purpose_of_membership' : kw.get('options')
             }
+            print(values)
+            # print(ans)
             #Create a new res.partner record
             partner = request.env['res.partner'].sudo().create(values)
 
         return request.render('website_account_extension.contact_form_comikha', {})
+    
+    def process_uploaded_file(self, file):
+        # Example function to process uploaded file data
+        if file:
+            # Read and process file content
+            file_data = file.read()
+            # You can save the file_data to database or perform other operations
+            return base64.b64encode(file_data).decode('utf-8')  # Return encoded file data for example
+        return None
--- website_account_extension/models/res_partner.py
+++ website_account_extension/models/res_partner.py
@@ -10,6 +10,7 @@ class ResPartner(models.Model):
     birth_place = fields.Char('Birth Place')
     occupation = fields.Char('Occupation')
     attachment_count = fields.Integer('Attachments')
+    purpose_of_membership = fields.Selection([('full_member', 'Full Member'), ('trader', 'Trader'), ('mining_operator', 'Artisnal Mining Operator')], string="Purpose Of MemberShip")
     membership_info = fields.Selection([
         ("supporting_honorary_member","Membre d'honneur de soutien"),
         ("full_member", "Membre effectif"),
